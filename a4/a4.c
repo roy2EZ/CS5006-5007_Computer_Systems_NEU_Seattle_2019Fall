b48ef881bee741639deb4584a8df4c48a5669baf
roychen <chen.rongy@husky.neu.edu>
2019-02-04 15:36:56 -0800

    adding a4 code

---
+++ a4/a4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h> 
+#include <string.h>
+
+#include "a4.h"
+
+#define PRINT_DEBUG 1
+
+// Implement the Hand and other functions in here
---
+++ a4/deck.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "a4.h"
+
+#define PRINT_DEBUG 1
+
+// Put your deck functions in here
+
