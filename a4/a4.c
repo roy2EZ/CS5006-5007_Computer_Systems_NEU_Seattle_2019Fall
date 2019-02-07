63b3d49dcf50aa1606d8edc2b60201f910658bae
roychen <chen.rongy@husky.neu.edu>
2019-02-06 17:53:31 -0800

    fix addCardToHand() and test it

--- a4/a4.c
+++ a4/a4.c
@@ -12,17 +12,36 @@
 Hand* createHand() {
   Hand* newHand = (Hand*)malloc(sizeof(newHand));
   newHand->num_cards_in_hand = 0;
+  newHand->firstCard = NULL;
   return newHand;
 }
 
-CardNode* createNode(Card* thisCard) {
-  CardNode* node = (CardNode*)malloc(sizeof(node));
-  node->nextCard = NULL;
-  node->prevCard = NULL;
-  node->thisCard = thisCard;
-  return node;
+void destroyHand(Hand* thisHand) {
+  free(thisHand->firstCard->nextCard);
+  free(thisHand->firstCard);
+  free(thisHand);
 }
+
+void addCardToHand(Card* card, Hand* thisHand) {
+  CardNode* thisCardNode = (CardNode*)malloc(sizeof(thisCardNode));
+  thisCardNode->thisCard = card;
+  thisCardNode->prevCard = NULL;
+  thisCardNode->nextCard = thisHand->firstCard;
+  if(thisHand->firstCard != NULL) {
+    thisHand->firstCard->prevCard = thisCardNode;
+    printHand(thisHand);
+  }
+  thisHand->firstCard = thisCardNode;
+  (thisHand->num_cards_in_hand)++;
+  printHand(thisHand);
+}
+// Removes a card from the hand. Return a pointer to the card that's been removed from the hand. Consider if need to remove the CardNode from the heap.
 /*
+struct card {
+  Name name;
+  Suit suit;
+  int value;
+};
 struct card_node {
   CardNode* nextCard;
   CardNode* prevCard;
@@ -34,20 +53,27 @@ struct hand {
   int num_cards_in_hand;
   CardNode* firstCard;
 };*/
-void destroyHand(Hand* thisHand) {
-  free(thisHand->firstCard->nextCard);
-  free(thisHand->firstCard);
-  free(thisHand);
-}
-
-void addCardToHand(Card* thisCard, Hand* thisHand) {
-  CardNode* thisCardNode = createNode(thisCard);
-  if(thisHand->firstCard != NULL) {
-    thisCardNode->nextCard = thisHand->firstCard;
-    thisHand->firstCard->prevCard = thisCardNode;
-  }
-  thisHand->firstCard = thisCardNode;
-  thisHand->num_cards_in_hand++;
-}
-
-
+/*
+Card* removeCardFromHand(Card* aCard, Hand* aHand) {
+  CardNode* tempNode = aHand->firstCard;
+  while(tempNode != NULL) {
+    // to find which node's card is as same as the aCard 
+    if (tempNode->thisCard->name != aCard->name || tempNode->thisCard->suit != aCard->suit) {
+    tempNode = tempNode->nextCard;
+    } else {
+      // start to remove
+      //case 1: the will-remove node is not the first of hand
+      if (tempNode->prevCard != NULL) {
+        tempNode->prevCard->nextCard = tempNode->nextCard;
+        tempNode->nextCard->prevCard = tempNode->prevCard;
+        return tempNode->thisCard;
+      } 
+      //case 2: the will-remove node is the first of hand
+      else {
+        aHand->firstCard = tempNode->nextCard;
+        aHand->firstCard->prevCard = NULL;
+        tempNode->nextCard=NULL;
+        return tempNode->thisCard;
+      }
+    }
+}*/
