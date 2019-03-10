9e2f7b4053e50d1344a97ce456aa5422bf3c9655
roychen <chen.rongy@husky.neu.edu>
2019-03-09 23:37:38 -0800

    finish system_info.sh

--- a6/system_info.sh
+++ a6/system_info.sh
@@ -3,10 +3,15 @@
 # Number of cores (includes those reported by hyper-threading cores)
 # Linux: Simply count the number of (non-comment) output lines from `lscpu -p`, 
 # which tells us the number of *logical* CPUs.
-coresCount=$([ $(uname) = 'Darwin' ] && 
-                       sysctl -n hw.logicalcpu_max || 
-                       lscpu -p | egrep -v '^#' | wc -l)
+coresCount=$([ $(uname) = 'Darwin' ] &&
+		 sysctl -n hw.logicalcpu_max ||
+		     lscpu -p | egrep -v '^#' | wc -l)
 
+freeMemory=$(free -m | awk 'NR==2{printf "%sMB(free)/%sMB(total) (%.2f%%)\n", $2-$3,$2,($2-$3)*100/$2 }')
+
+freeStorage=$(df -h | awk '$NF=="/"{printf "Disk Usage: %dGB/%dGB (%.2f%%)\n", $2-$3,$2,(100-$5)}')
 cat <<EOF
 # cores: $coresCount
+# memory: $freeMemory
+# storage: $freeStorage 
 EOF
