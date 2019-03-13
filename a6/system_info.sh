8699d2656e6dbd8e4450cdc9dcf2407fa4e12ec2
roychen <chen.rongy@husky.neu.edu>
2019-03-13 11:17:23 -0700

    update ex1

--- a6/system_info.sh
+++ a6/system_info.sh
@@ -2,16 +2,16 @@
 # author:Rongyi Chen
 # date:3/9/2019
 
-# Number of cores (includes those reported by hyper-threading cores)
-# Linux: Simply count the number of (non-comment) output lines from `lscpu -p`, 
-# which tells us the number of *logical* CPUs.
+# get the number of cores 
 coresCount=$([ $(uname) = 'Darwin' ] &&
 		 sysctl -n hw.logicalcpu_max ||
 		     lscpu -p | egrep -v '^#' | wc -l)
-
+# get the free memory
 freeMemory=$(free -m | awk 'NR==2{printf "%sMB(free)/%sMB(total) (%.2f%%)\n", $2-$3,$2,($2-$3)*100/$2 }')
 
+# get the free space in storage
 freeStorage=$(df -h | awk '$NF=="/"{printf "%dGB(free)/%dGB(total) (%.2f%%)\n", $2-$3,$2,(100-$5)}')
+
 cat <<EOF
 # cores: $coresCount
 # memory: $freeMemory
