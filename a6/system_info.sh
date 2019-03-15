07fcc2c1ac355583fe2c391086319741dfed0b27
roychen <chen.rongy@husky.neu.edu>
2019-03-15 00:11:25 -0700

    update system_info.sh

--- a6/system_info.sh
+++ a6/system_info.sh
@@ -8,10 +8,10 @@
 # get the number of cores 
 coresCount=$(nproc)
 # get the free memory
-freeMemory=$(free -m | awk 'NR==2{printf "%s MB(free)/%s MB(total)\n", $2-$3,$2 }')
+freeMemory=$(free -m | awk 'NR==2{printf "%s MB(free) / %s MB(total)\n", $2-$3,$2 }')
 
 # get the free space in storage
-freeStorage=$(df -h | awk '$NF=="/"{printf "%d GB(free)/%d GB(total)\n", $2-$3,$2 }')
+freeStorage=$(df -h | awk '$NF=="/"{printf "%d GB(free) / %d GB(total)\n", $2-$3,$2 }')
 
 cat <<_EOF_
   cores: $coresCount
