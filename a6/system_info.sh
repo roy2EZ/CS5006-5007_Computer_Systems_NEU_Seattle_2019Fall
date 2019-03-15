49ac1c3c29e6712f93c0efb014078d2e6b23d76d
roychen <chen.rongy@husky.neu.edu>
2019-03-14 23:55:08 -0700

    update system_info.sh

--- a6/system_info.sh
+++ a6/system_info.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 # author:Rongyi Chen
 # date:3/9/2019
+# reference:
+# https://stackoverflow.com/a/23378780
+# https://unix.stackexchange.com/a/119170
 
 # get the number of cores 
 coresCount=$([ $(uname) = 'Darwin' ] &&
 		 sysctl -n hw.logicalcpu_max ||
 		     lscpu -p | egrep -v '^#' | wc -l)
 # get the free memory
-freeMemory=$(free -m | awk 'NR==2{printf "%sMB(free)/%sMB(total) (%.2f%%)\n", $2-$3,$2,($2-$3)*100/$2 }')
+freeMemory=$(free -m | awk 'NR==2{printf "%s MB(free)/%s MB(total)\n", $2-$3,$2 }')
 
 # get the free space in storage
-freeStorage=$(df -h | awk '$NF=="/"{printf "%dGB(free)/%dGB(total) (%.2f%%)\n", $2-$3,$2,(100-$5)}')
+freeStorage=$(df -h | awk '$NF=="/"{printf "%d GB(free)/%d GB(total)\n", $2-$3,$2 }')
 
-cat <<EOF
-# cores: $coresCount
-# memory: $freeMemory
-# storage: $freeStorage 
-EOF
+cat <<_EOF_
+  cores: $coresCount
+  memory: $freeMemory
+  storage: $freeStorage 
+_EOF_
