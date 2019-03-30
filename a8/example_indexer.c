0030663df124c28a68223c594b44cc27b21b40dc
roychen <chen.rongy@husky.neu.edu>
2019-03-30 15:05:22 -0700

    add start code of a8

---
+++ a8/example_indexer.c
@@ -0,0 +1,45 @@
+/*
+ *  Created by Adrienne Slaughter
+ *  CS 5007 Spring 2019
+ *  Northeastern University, Seattle
+ *
+ *  This is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  It is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  See <http://www.gnu.org/licenses/>.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "htll/LinkedList.h"
+#include "MovieIndex.h"
+#include "Movie.h"
+#include "MovieReport.h"
+#include "FileParser.h"
+
+
+int main(int argc, char* argv[]) {
+
+  // TODO: Parse command-line args to index based on the correct field (see getopt)
+
+  LinkedList movie_list  = ReadFile(filename);
+
+  
+  // TODO: Output report to file, rather than terminal (see MovieReport.h)
+  PrintReport(index);
+
+  DestroyTypeIndex(index);
+ 
+  return 0;
+}
+
